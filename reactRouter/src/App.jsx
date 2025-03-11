680f62ca9fd786a08bd18f9abd09dbf5718d0477
Amit Paudel <amitpaudel789@gmail.com>
2025-03-11 18:29:29 +0545

    react-router

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-black text-white text-center py-8 w-[50%] ">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
