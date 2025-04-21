d13abf17c694d35353187f9698c804d0482f8ba3
Amit Paudel <amitpaudel789@gmail.com>
2025-04-21 18:25:06 +0545

    localStorage

--- todoPractice/src/App.jsx
+++ todoPractice/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
 import { TodoProvider } from "./contexts";
@@ -17,6 +17,18 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    console.log(todos);
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo }}>
