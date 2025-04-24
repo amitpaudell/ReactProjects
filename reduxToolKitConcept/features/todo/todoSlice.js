0b6b943015d3c80e54821aa689c64bb3da05bd50
Amit Paudel <amitpaudel789@gmail.com>
2025-04-24 19:52:08 +0545

    redux setup

--- reduxToolKitConcept/features/todo/todoSlice.js
+++ reduxToolKitConcept/features/todo/todoSlice.js
@@ -18,12 +18,12 @@ export const todoSlice = createSlice({
     },
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => {
-        todo.id !== action.payload;
+        return todo.id !== action.payload;
       });
     },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.action;
+export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
